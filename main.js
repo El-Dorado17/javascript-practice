99034f82242f6d866a831966bf952566502b767c
Ely Dorado <2018dorado@gmail.com>
2023-07-11 08:40:35 -0500

    first challenge complete

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ Initial push
 */
 
 //This change was made on work branch!
+
+
+//1.) Return the sum of 2 numbers
+const addition = (a, b) =>{
+    let sum = a + b
+    return sum
+}
+console.log(addition(2,4))
