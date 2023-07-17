8c3600b18aa9b7ec94ec1a906b79ec11791edbf1
Ely Dorado <2018dorado@gmail.com>
2023-07-17 18:31:28 -0500

    #2

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-
-
 //1.) Return the sum of 2 numbers
 const addition = (a, b) =>{
     let sum = a + b
     return sum
 }
-console.log(addition(2,4))
+//console.log(addition(2,4))
+
+//2.) Function that converts mins to seconds
+const minToSec = (x) => {
+    conversion = x * 60
+    return conversion
+}
+console.log(minToSec(60))
